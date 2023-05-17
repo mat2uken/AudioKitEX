730c5a505c71917a84e281a8d19a260ed5ff8fe6
mat2uken <Kenichi.A.Matsumoto@roland.com>
2023-05-17 14:48:09 +0900

    replace Package.swift

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "AudioKitEX",
     platforms: [.macOS(.v12), .iOS(.v13), .tvOS(.v13)],
     products: [.library(name: "AudioKitEX", targets: ["AudioKitEX"])],
-    dependencies: [.package(url: "https://github.com/AudioKit/AudioKit", from: "5.5.0")],
+    dependencies: [.package(url: "https://github.com/mat2uken/AudioKit", from: "v1.5.8-ae")],
     targets: [
         .target(name: "AudioKitEX", dependencies: ["AudioKit", "CAudioKitEX"]),
         .target(name: "CAudioKitEX", cxxSettings: [.headerSearchPath(".")]),
